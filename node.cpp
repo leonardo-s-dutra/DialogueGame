f0946a99353961b9add4e6cda9907780815a1ba4
Leonardo Silva Dutra <leo.dutra012@gmail.com>
2020-05-23 15:41:02 -0300

    Implemented Node functionalities.

--- node.cpp
+++ node.cpp
@@ -1,11 +1,41 @@
 #include "node.h"
 
-Node::Node(QObject *parent) : QObject(parent)
+Node::Node(QObject* parent) : QObject(parent)
 {
 
 }
 
-Node::~Node()
+int Node::hasOptions()
 {
+    return Options.count() > 0;
+}
 
+int Node::id() const
+{
+    return this->Id;
+}
+
+QString Node::text() const
+{
+    return this->Text;
+}
+
+QList<Option*> Node::options() const
+{
+    return this->Options;
+}
+
+void Node::setID(int Id)
+{
+    this->Id = Id;
+}
+
+void Node::setText(QString text)
+{
+    this->Text = text;
+}
+
+void Node::addOption(Option* option)
+{
+    this->Options.append(option);
 }
--- node.h
+++ node.h
@@ -1,12 +1,26 @@
 #pragma once
 
 #include <QObject>
+#include "option.h"
 
 class Node : public QObject
 {
-	Q_OBJECT
-
+    Q_OBJECT
 public:
-	Node(QObject *parent);
-	~Node();
+    explicit Node(QObject* parent = nullptr);
+
+    int hasOptions();
+
+    int id() const;
+    QString text() const;
+    QList<Option*> options() const;
+
+    void setID(int ID);
+    void setText(QString text);
+    void addOption(Option* option);
+
+private:
+    int Id;
+    QString Text;
+    QList<Option*> Options;
 };
