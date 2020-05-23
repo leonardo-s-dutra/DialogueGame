97b2e88fca99b55accab2824a033e1e1c74d3e3f
Leonardo Silva Dutra <leo.dutra012@gmail.com>
2020-05-23 16:00:34 -0300

    Implemented DialogueManager functionalities.

--- gamescreen.cpp
+++ gamescreen.cpp
@@ -2,10 +2,122 @@
 
 GameScreen::GameScreen(QWidget *parent) : QWidget(parent)
 {
+    dialogueWidget  = new DialogueWidget    (this);
+    optionsWidget   = new OptionsWidget     (this);
+    dialogueManager = new DialogueManager   (this);
+    ReturnButton    = new QPushButton       ("Return", this);
+    stackedWidget   = new QStackedWidget    (this);
 
+    dialogueWidget->setMinimumHeight(400);
+    ReturnButton->setFixedSize(120, 40);
+    ReturnButton->setObjectName("Menu");
+
+    QWidget *returnButtonWidget = new QWidget(this);
+    returnButtonWidget->setLayout(new QVBoxLayout(this));
+    returnButtonWidget->layout()->setAlignment(Qt::AlignCenter);
+    returnButtonWidget->layout()->addWidget(ReturnButton);
+
+    stackedWidget->addWidget(optionsWidget);
+    stackedWidget->addWidget(returnButtonWidget);
+
+    QSplitter *splitter = new QSplitter(Qt::Vertical, this);
+
+    splitter->addWidget(dialogueWidget);
+    splitter->addWidget(stackedWidget);
+
+    splitter->setCollapsible(0, false);
+    splitter->setCollapsible(1, false);
+
+    setLayout(new QVBoxLayout(this));
+    layout()->addWidget(splitter);
+
+    connect(optionsWidget->buttons(), SIGNAL(buttonClicked(int)), this, SLOT(update(int)));
 }
 
-GameScreen::~GameScreen()
+void GameScreen::init()
 {
+    stackedWidget->setCurrentIndex(0);
 
+    Node *firstNode = dialogueManager->getNode(0);
+    QString firstText = firstNode->text();
+
+    dialogueWidget->initText(firstText);
+
+    optionsWidget->loadButtons(firstNode);
+}
+
+void GameScreen::finish()
+{
+    dialogueWidget->finishText();
+
+    dialogueManager->deallocate();
+
+    stackedWidget->setCurrentIndex(1);
+}
+
+void GameScreen::update(int id)
+{
+    Node* currentNode = dialogueManager->getNode(id);
+
+    QString currentText = currentNode->text();
+    QString selectedText = optionsWidget->buttons()->button(id)->text();
+
+    dialogueWidget->updateText(currentText, selectedText);
+
+    optionsWidget->clearButtons();
+
+    if (!currentNode->hasOptions())
+        finish();
+
+    else
+        optionsWidget->loadButtons(currentNode);
+}
+
+void GameScreen::loadDialogue(QString path)
+{
+    switch (dialogueManager->load(path))
+    {
+    case INVALID_FILE:
+        emit loadFailed("Dialogue must be a .xml file.");
+        break;
+
+    case INVALID_CONTENT:
+        emit loadFailed("Could not set content to dialogue.");
+        break;
+
+    case NO_NODE_ID:
+        emit loadFailed("Node with missing ID found.");
+        break;
+
+    case NO_NODE_TEXT:
+        emit loadFailed("Node with missing text found.");
+        break;
+
+    case NO_OPTION_TEXT:
+        emit loadFailed("Option with missing text found.");
+        break;
+
+    case NO_OPTION_DESTINATION_ID:
+        emit loadFailed("Option with missing destination ID found.");
+        break;
+
+    case OK:
+        emit loadOk();
+        break;
+    }
+}
+
+DialogueWidget *GameScreen::getDialogueWidget() const
+{
+    return dialogueWidget;
+}
+
+OptionsWidget *GameScreen::getOptionsWidget() const
+{
+    return optionsWidget;
+}
+
+QPushButton *GameScreen::returnButton() const
+{
+    return ReturnButton;
 }
--- gamescreen.h
+++ gamescreen.h
@@ -1,15 +1,40 @@
 #pragma once
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QStackedWidget>
+#include <QSplitter>
+#include "dialoguewidget.h"
+#include "optionswidget.h"
+#include "dialoguemanager.h"
+
 
 class GameScreen : public QWidget
 {
-	Q_OBJECT
-
+    Q_OBJECT
 public:
-	GameScreen(QWidget *parent = Q_NULLPTR);
-	~GameScreen();
+    explicit GameScreen(QWidget *parent = nullptr);
+
+    void init();
+    void finish();
+
+    DialogueWidget* getDialogueWidget() const;
+    OptionsWidget* getOptionsWidget() const;
+
+    QPushButton* returnButton() const;
+
+public slots:
+    void loadDialogue(QString path);
+    void update(int id);
+
+signals:
+    void loadFailed(QString status);
+    void loadOk();
 
 private:
-
+    DialogueWidget *dialogueWidget;
+    OptionsWidget *optionsWidget;
+    DialogueManager *dialogueManager;
+    QPushButton *ReturnButton;
+    QStackedWidget *stackedWidget;
 };
