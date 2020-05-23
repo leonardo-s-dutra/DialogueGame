72a6e93d5975d5f061a7ecf2e114b1dc8ac2c4a9
Leonardo Silva Dutra <leo.dutra012@gmail.com>
2020-05-23 16:15:52 -0300

    Addes stylesheet file.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile styleSheetFile(":/styles/styleSheet.css");
+    styleSheetFile.open(QFile::ReadOnly);
+
+    a.setStyleSheet(styleSheetFile.readAll());
+
     MainWindow w;
+
     w.show();
     return a.exec();
 }
